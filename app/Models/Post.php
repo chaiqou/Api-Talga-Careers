8de975ad733ef74878ce60c9dfad9470b209bc63
chaiqou <lomtadzenikusha@gmail.com>
2022-08-16 14:07:43 +0400

    chore: mutator and accessor examples

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,8 +17,27 @@ class Post extends Model
         'body' => 'array',
     ];
 
+    // Accessor უნდა იწყებოდეს გეთით და მთავრდებოდეს ატრიბუტით მასზე წვდომა გვაქვს შემდეგნაირად title_upper_case
+    // Accessor მეშვეობით ბაზიდან მოგვაქვს რაიმე და შეგვიძლია მისი ისე დამუშავება როგორც ჩვენ გვინდა
+    public function getTitleUpperCaseAttribute()
+    {
+        return strtoupper($this->title);
+    }
+
+    // Mutator უნდა იწყებოდეს სეტით და მთავრდებოდეს ატრიბუტით
+    // Mutator სანამ ბაზაში შევინახავთ მანამდე ცვლის მნიშვნელობას
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = strtolower($value);
+    }
+
     public function comments()
     {
         return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'post_user', 'user_id', 'post_id');
+    }
 }
