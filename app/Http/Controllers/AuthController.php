5475434caa2b215c553f3793bc2f762eb169764c
chaiqou <lomtadzenikusha@gmail.com>
2022-08-24 18:41:36 +0400

    feat: create registration with sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegistrationRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+  ]);
+
+        $token = $user->createToken('chemiaplikaciistokeni')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+
+        return response($response, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use App\Models\Job;
 use App\Models\User;
 use Illuminate\Database\Console\DumpCommand;
@@ -23,3 +25,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('posts', PostController::class);
 Route::apiResource('jobs', JobController::class);
+Route::post('registration', [AuthController::class, 'register'])->name('user.registration');
+
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', [UserController::class, 'index'])->name('all.user');
+});
